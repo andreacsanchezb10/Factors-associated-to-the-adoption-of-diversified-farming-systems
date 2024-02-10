878ffd0d839cd01cab6f201ebae1bb0591ad6bb0
Andrea Sanchez <andreacsanchezb@gmail.com>
2024-02-10 17:20:15 +0100

    f

--- 2_Effect_size_calculation.R
+++ 2_Effect_size_calculation.R
@@ -9,9 +9,7 @@ factors_metric_assessed <- read_excel("C:/Users/AndreaSanchez/OneDrive - CGIAR/1
 factors_metric_assessed$pcc_factor_unit <- paste(factors_metric_assessed$x_metric_recla2,
                                                  " (",factors_metric_assessed$pcc_unit,")", sep="")
 
-
-data<-read.csv("binary_adoption_clean_data.csv",
-               header = TRUE, sep = ",")%>%
+data<-read.csv("binary_adoption_clean_data.csv",header = TRUE, sep = ",")%>%
   select( "article_id","model_id", "main_crop" ,"country",                        
           "intervention_recla","intervention_recla_detail_1" ,   
           "intervention_recla_detail_2", "intervention_recla_detail_3",    "intervention_recla_detail_4",     "y_metric_recla" ,                
@@ -27,7 +25,7 @@ data<-read.csv("binary_adoption_clean_data.csv",
           "b_logOR"        ,                 "se_logOR"    ,"m_region"    ,"m_sub_region",                 
           "Developed_Developing" ,         
          "m_intervention_recla2" ,"m_exact_variance_value","m_random_sample", "m_mean_farm_size_ha",
-        "m_sampling_unit", "m_type_data", "m_av_year_assessment")%>%
+        "m_sampling_unit", "m_type_data", "m_av_year_assessment", "m_education_years")%>%
   left_join(factors_metric_assessed, by= c("factor_metric"))
 
 sort(unique(data$factor_metric))
@@ -39,41 +37,16 @@ names(data)
 length(unique(data$article_id)) #153 articles for PCC analysis
 sort(unique(data$article_id))  
 sort(unique(data$pcc_factor_unit))  
-length(unique(data$pcc_factor_unit)) #68
+length(unique(data$pcc_factor_unit)) #70
 
 length(unique(data$m_intervention_recla2)) #10 systems
 sort(unique(data$country)) #44
 sort(unique(data$limitation_of_use_obs))
 
 #######  Included FACTORS -------
-length(unique(data$x_metric_recla)) #46
+length(unique(data$x_metric_recla)) #48
 sort(unique(data$x_metric_recla))
 length(unique(data$x_metric_recla2))#49
-sort(unique(data$x_metric_recla2))#47
-
-[1] "access to agricultural extension"                           "access to agricultural information"                        
-[3] "access to agricultural training"                            "access to credit"                                          
-[5] "access to credit is a constraint"                           "access to irrigation"                                      
-[7] "access to off-farm income"                                  "agricultural extension frequency"                          
-[9] "awareness of SFP or DFS"                                    "distance farm-house"                                       
-[11] "distance to input market"                                   "distance to market"                                        
-[13] "distance to output market"                                  "family members and friends living in and out the community"
-[15] "farm labour force (hired)"                                  "farm labour force (non-hired)"                             
-[17] "farm size"                                                  "h adult members"                                           
-[19] "h income"                                                   "h off-farm income"                                         
-[21] "h on-farm income"                                           "h size"                                                    
-[23] "hh age"                                                     "hh association member"                                     
-[25] "hh comunicate with other farmers"                           "hh education"                                              
-[27] "hh farming experience"                                      "hh gender"                                                 
-[29] "hh is native"                                               "hh marital status"                                         
-[31] "hh perceive benefits of SFP or DFS"                         "hh risk attitude"                                          
-[33] "land tenure security"                                       "limitations to implement SFT or DFS"                       
-[35] "livestock owned"                                            "number of agricultural plots"                              
-[37] "plot size"                                                  "precipitation"                                             
-[39] "production constraints"                                     "receive incentive for conservation"                        
-[41] "soil depth"                                                 "soil fertility"                                            
-[43] "soil slope"                                                 "temperature"                                               
-[45] "units of livestock"  
 
 str(data)
 sort(unique(data$factor_metric))
@@ -86,7 +59,7 @@ factors_metric_unit<-data%>%
 
 sort(unique(factors_metric_unit$pcc_factor_unit)) # 135 factor_metric
 
-write.csv(factors_metric_unit, "C:/Users/andreasanchez/OneDrive - CGIAR/1_chapter_PhD/meta-analysis/adoption_meta_analysis_2024.02.04/Factors-associated-to-the-adoption-of-diversified-farming-systems/pcc_factors_metric_unit_articles.csv", row.names=FALSE)
+write.csv(factors_metric_unit, "pcc_factors_metric_unit_articles.csv", row.names=FALSE)
 
 ####################### EFFECT SIZE calculation---------------------------------
 library(metafor)
@@ -108,5 +81,5 @@ check_meta<- pcc_data%>%
 
 sort(unique(pcc_data$pcc_unit))
 
-write.csv(pcc_data, "C:/Users/andreasanchez/OneDrive - CGIAR/1_chapter_PhD/meta-analysis/adoption_meta_analysis_2024.02.04/Factors-associated-to-the-adoption-of-diversified-farming-systems/pcc_data.csv", row.names=FALSE)
+write.csv(pcc_data, "pcc_data.csv", row.names=FALSE)
 
